a2a33fbd10e22e0b4bb3617c2e360dfadda23727
Venkata Mahesh Chukka <venkata@backbase.com>
2020-03-09 09:39:07 -0400

    adding all my initial files

---
+++ app/src/main/java/com/androidtutz/anushka/moviesapp/model/MovieRepository.java
@@ -0,0 +1,84 @@
+package com.androidtutz.anushka.moviesapp.model;
+
+import android.app.Application;
+import android.arch.lifecycle.MutableLiveData;
+
+import com.androidtutz.anushka.moviesapp.R;
+import com.androidtutz.anushka.moviesapp.service.MoviesDataService;
+import com.androidtutz.anushka.moviesapp.service.RetrofitInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+
+public class MovieRepository {
+    private Application application;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    public MutableLiveData<List<Movie>> getMoviesLiveData() {
+        return moviesLiveData;
+    }
+
+    public void setMoviesLiveData(MutableLiveData<List<Movie>> moviesLiveData) {
+        this.moviesLiveData = moviesLiveData;
+    }
+
+    private MutableLiveData<List<Movie>> moviesLiveData = new MutableLiveData<>();
+    private ArrayList<Movie> movies;
+    private Observable<MovieDBResponse> movieDBResponseObservable;
+
+
+    public MovieRepository(Application application) {
+        this.application = application;
+        getPopularMoviesWithRx();
+
+    }
+
+    public void getPopularMoviesWithRx() {
+        movies = new ArrayList<>();
+        MoviesDataService getMoviesDataService = RetrofitInstance.getService();
+        movieDBResponseObservable = getMoviesDataService.getPopularMoviesWithRx(application.getResources().getString(R.string.api_key));
+
+        compositeDisposable.add(movieDBResponseObservable.subscribeOn(Schedulers.io()).
+                observeOn(AndroidSchedulers.mainThread())
+                .flatMap(new Function<MovieDBResponse, Observable<Movie>>() {
+                    @Override
+                    public Observable<Movie> apply(MovieDBResponse movieDBResponse) throws Exception {
+                        return Observable.fromArray(movieDBResponse.getMovies().toArray(new Movie[0]));
+                    }
+                })
+                .filter(new Predicate<Movie>() {
+                    @Override
+                    public boolean test(Movie movie) throws Exception {
+                        return movie.getVoteAverage() > 7.0;
+                    }
+                }).subscribeWith(new DisposableObserver<Movie>() {
+                    @Override
+                    public void onNext(Movie movie) {
+                        movies.add(movie);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        moviesLiveData.postValue(movies);
+                    }
+                }));
+    }
+
+    public void clear() {
+        compositeDisposable.clear();
+    }
+}
---
+++ app/src/main/java/com/androidtutz/anushka/moviesapp/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,31 @@
+package com.androidtutz.anushka.moviesapp.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.androidtutz.anushka.moviesapp.model.Movie;
+import com.androidtutz.anushka.moviesapp.model.MovieRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    private Application application;
+    private MovieRepository movieRepository;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.application = application;
+        movieRepository = new MovieRepository(application);
+    }
+
+    public LiveData<List<Movie>> getAllMovies() {
+        return movieRepository.getMoviesLiveData();
+    }
+
+    public void clear() {
+        movieRepository.clear();
+    }
+}
